05c9bec345b18eb80624e87acec863b68dc9558e
whittew <lexa03168@gmail.com>
2023-03-29 01:43:03 +0300

    страницы с задачами, ломаный калькулятор

--- myproject/app1/views.py
+++ myproject/app1/views.py
@@ -4,3 +4,43 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'app1/index.html')
+
+
+def task1(request):
+    return render(request, 'app1/task1.html')
+
+
+def task2(request):
+    return render(request, 'app1/task2.html')
+
+
+def task3(request):
+    return render(request, 'app1/task3.html')
+
+# Реализация решения task1
+
+
+def task1_way(v, a):
+    v = int(v)/3.6
+    a = int(a)/10
+    S = (v**2) / (2 * a)
+    return S
+
+
+def task1_time(v, a):
+    v = int(v)/3.6
+    a = int(a)/10
+    time = v/a
+    return time
+
+
+def task1_output(request):
+    if request.method == 'POST':
+        v = request.POST['v']
+        a = request.POST['a']
+        if 'div' in request.POST:
+            way = task1_way(v, a)
+            time = task1_time(v, a)
+            return render(request, 'task1.html', {'Путь': way}, {'Время': time})
+
+    return render(request, 'task1.html')
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from app1.views import index
+from app1.views import index, task1, task2, task3
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index)
+    path('', index),
+    path('task1/', task1),
+    path('task2/', task2),
+    path('task3/', task3)
 ]
